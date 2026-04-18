8beb596587e9533fdc3bc62298ccca694fa8c0ab
damodred53 <florent.guyard@live.fr>
2026-04-18 23:38:17 +0200

    création page d'erreur

--- src/Pages/Error/Error.tsx
+++ src/Pages/Error/Error.tsx
@@ -1,9 +1,11 @@
+import TitleTextBloc from "../../Components/CommonComponents/TitleTextBloc/TitleTextBloc";
+import styles from "./Error.module.scss";
 
 const Error = () => {
   return (
-    <div>   
-        <h1>Erreur</h1>
-        <p>404 - Page non trouvée</p>
+    <div className={styles.wrapper}>
+        <p className={styles.title}>404</p>
+        <TitleTextBloc className={styles.textBloc} titleOrientation="middle" title="Oups ! Page introuvable" text="La page que vous recherchez a sauté... comme un bouchon de bouteille !" />
     </div>
   );
 }
